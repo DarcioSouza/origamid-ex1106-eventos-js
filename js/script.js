d68b0872135efac9826dcc76d1de642a2ff23b51
DarcioSouza <darciorsouza@gmail.com>
2023-06-25 21:27:10 -0300

    simplificacao do codigo js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,8 @@
+const circulo = document.querySelector(".circulo");
+
 function seguirMouse(event) {
-  const x = event.x;
-  const y = event.y;
-  const circulo = document.querySelector(".circulo");
-  circulo.style.top = y + "px";
-  circulo.style.left = x + "px";
+  circulo.style.top = event.y + "px";
+  circulo.style.left = event.x + "px";
 }
 
 window.addEventListener("mousemove", seguirMouse);
